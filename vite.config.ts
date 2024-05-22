51e3ba072caab59606e43ac47c0d8bcd0d895690
silverwind <me@silverwind.io>
2024-05-22 22:39:37 +0200

    use shared vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,6 @@
 import {defineConfig} from "vite";
-import {fileURLToPath} from "node:url";
-import dtsPlugin from "vite-plugin-dts";
-import {builtinModules} from "node:module";
+import {lib} from "vite-config-silverwind";
 
-export default defineConfig({
-  build: {
-    outDir: fileURLToPath(new URL("dist", import.meta.url)),
-    minify: false,
-    sourcemap: false,
-    target: "modules",
-    emptyOutDir: true,
-    chunkSizeWarningLimit: Infinity,
-    assetsInlineLimit: 0,
-    reportCompressedSize: false,
-    lib: {
-      entry: [fileURLToPath(new URL("index.ts", import.meta.url))],
-      formats: ["es"],
-    },
-    rollupOptions: {
-      external: builtinModules.map(module => `node:${module}`),
-    }
-  },
-  plugins: [
-    dtsPlugin({exclude: [
-      "*.config.*",
-      "*.test.*",
-    ]}),
-  ],
-});
+export default defineConfig(lib({
+  url: import.meta.url,
+}));
